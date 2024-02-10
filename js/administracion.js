47cb8f047087ceb4a34b1ec6a60f89f5c894e3a1
tomichiruss <lukadayo03@gmail.com>
2024-02-10 04:31:20 -0300

    Arreglado array de juegos

--- js/administracion.js
+++ js/administracion.js
@@ -1,4 +1,7 @@
 function destacar(destacarId){
+    localStorage.setItem('juegoDestacado', destacarId);
+    let destacarJuego = localStorage.getItem('juegoDestacado');
+    console.log(destacarJuego);
     filas = JSON.parse(localStorage.getItem('filas'));
     let filaDestacar = document.getElementById(destacarId);
     for(let i=0; i<filas.length; i++){
@@ -175,53 +178,92 @@ var filas = [
     {
         id: 'lethal-row',
         codigo: '123',
-        clase:'table-success',
+        clase:'',
         nombre: 'Lethal Company',
         categoria: 'Terror',
         descripcion: 'Lootear y conseguir materiales para la compañía.',
-        checked: 'checked',       
+        descripcionLarga: 'Eres un trabajador contratado por la Compañía. Tu trabajo consiste en recoger chatarra de lunas abandonadas e industrializadas para cumplir la cuota de beneficios de la Compañía. Puedes usar el dineroque ganes para viajar a nuevas lunas con mayores riesgos y recompensas...',
+        checked: 'checked',
+        img: '/assets/images/header.jpg',
+        iframe:'https://www.youtube.com/embed/qQFUol6k-ig?si=Pr8DyYhFi4oPVQmK',
+        img1: '/assets/images/lc1.jpg',
+        img2: '/assets/images/lc2.jpg',
+        img3: '/assets/images/lc3.jpg',
+        img4: '/assets/images/lc4.jpg',            
     },
     
     {
-        id: 'csgo-row',
+        id: 'battlefield-row',
         codigo: '124',
         clase:'',
-        nombre: 'CSGO',
-        categoria: 'FPS',
+        nombre: 'BATTLEFIELD',
+        categoria: 'Acción',
         descripcion: 'disparos y terrorismo.',
-        checked: 'checked',      
+        descripcionLarga: 'Domina lo desconocido en la temporada 6. Battlefield™ 2042 es un juego del tipo FPS',
+        checked: 'checked',
+        img:'https://cdn.akamai.steamstatic.com/steam/apps/1517290/header.jpg',
+        iframe:'https://www.youtube.com/embed/WomAGoEh-Ss?si=tVPki-934v5xgZJB',
+        img1: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1517290/ss_5b422e52477416a027dc3a4373cdd5332a1f341f.600x338.jpg?t=1703193378',
+        img2: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1517290/ss_a7ebde7fe03f57e06e53f830b1345a3af9279864.600x338.jpg?t=1703193378',
+        img3: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1517290/ss_4c3e1fbdc6c02ba04fc6b37bba55a444fa4e11b4.600x338.jpg?t=1703193378',
+        img4: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1517290/ss_a09922fccf91b6842577511fa04459b70725d6b1.600x338.jpg?t=1703193378',              
     },
 
     {
-        id: 'rocket-row',
+        id: 'fc24-row',
         codigo: '125',
         clase:'',
-        nombre: 'Rocket League',
+        nombre: 'FC24',
         categoria: 'Deportes',
-        descripcion: 'Autos voladores y fútbo.l',
-        checked: 'checked',        
+        descripcion: 'Que viva el fulbol.',
+        descripcionLarga: '¡Asegurate de anotar muchos goles para ganar a todos y ser el mejor del mundo!',
+        checked: 'checked',
+        img:'https://cdn.akamai.steamstatic.com/steam/apps/2195250/header.jpg',
+        iframe:'https://www.youtube.com/embed/-vL01jbgENE?si=rEncnWIOJVZfCA6S',
+        img1: 'https://cdn.cloudflare.steamstatic.com/steam/apps/2195250/ss_33527a88323cbb39a7c09d7d74955015343efd3f.600x338.jpg',
+        img2: 'https://cdn.cloudflare.steamstatic.com/steam/apps/2195250/ss_96496b17bd35a97ceb926231e963762d3b86c3c8.600x338.jpg',
+        img3: 'https://cdn.cloudflare.steamstatic.com/steam/apps/2195250/ss_6c1ef918ffe94050e72dcb2cfad3a95ee3d69f08.600x338.jpg',
+        img4: 'https://cdn.cloudflare.steamstatic.com/steam/apps/2195250/ss_f3d31c7cffc2a6ab42fb76aaac2bc9d5258bc142.600x338.jpg',
     },
 
     {
-        id: 'lol-row',
-        codigo: '126',
-        clase:'',
-        nombre: 'League of Legends',
-        categoria: 'MOBA',
-        descripcion: '5 conos contra 5 conos.',
-        checked: 'checked',  
-    },
-
-    {
-        id: 'outlast-row',
+        id: 'hogwarts-row',
         codigo: '127',
         clase:'',
-        nombre: 'Outlast',
-        categoria: 'Terror',
-        descripcion: 'Persecusiones en un manicomio.',
+        nombre: 'Hogwarts Legacy',
+        categoria: 'Rol',
+        descripcion: 'Adentrate en el mundo de la magia.',
+        descripcionLarga: 'Hogwarts Legacy es un RPG de acción en un mundo abierto. Ahora puedes tomar el control de la acción y ser el centro de tu propia aventura en el mundo mágico.',
         checked: 'checked',
+        img:'https://cdn.akamai.steamstatic.com/steam/apps/990080/header.jpg',
+        iframe:'https://www.youtube.com/embed/S6GTl_vPRvU?si=qx6qzpBs1tElXReo',
+        img1: 'https://cdn.cloudflare.steamstatic.com/steam/apps/990080/ss_725bf58485beb4aa37a3a69c1e2baa69bf3e4653.600x338.jpg',
+        img2: 'https://cdn.cloudflare.steamstatic.com/steam/apps/990080/ss_8e08976236d29b1897769257ac3c64e9264792a5.600x338.jpg',
+        img3: 'https://cdn.cloudflare.steamstatic.com/steam/apps/990080/ss_df93b5e8a183f7232d68be94ae78920a90de1443.600x338.jpg',
+        img4: 'https://cdn.cloudflare.steamstatic.com/steam/apps/990080/ss_94058497bf0f8fabdde17ee8d59bece609a60663.600x338.jpg',
+        
     },
+
+    {
+        id: 'gtavi-row',
+        codigo: '165',
+        clase:'table-success',
+        nombre: 'GTA VI',
+        categoria: 'Acción',
+        descripcion: 'Vive la vida loca.',
+        descripcionLarga: 'Grand Theft Auto VI es un videojuego de acción-aventura de mundo abierto desarrollado distribuido por Rockstar Games. Será la octava entrega principal de dicha serie, la cual seráestrenada en el año 2025.',
+        checked: 'checked',
+        img:'https://grupovierci.brightspotcdn.com/dims4/default/b4a04f6/2147483647/strip/false/crop/1920x1080+0+0/resize/1486x836!/quality/90/?url=https%3A%2F%2Fk2-prod-grupo-vierci.s3.us-east-1.amazonaws.com%2Fbrightspot%2F87%2Fb1%2F502a59b94b4da2c7c505ed4ea985%2Fgta-6.jfif',
+        iframe: 'https://www.youtube.com/embed/QdBZY2fkU-0?si=26OnQF0YdHKoQDUS',
+        img1: 'https://sm.ign.com/t/ign_es/news/e/ex-rocksta/ex-rockstar-dev-reacts-to-gta-6-trailer-its-really-going-to_rbvh.960.jpg',
+        img2: 'https://sm.ign.com/t/ign_es/photo/e/ex-rocksta/ex-rockstar-dev-reacts-to-gta-6-trailer-its-really-going-to_ju9s.960.jpg',
+        img3: 'https://media.tycsports.com/files/2023/12/04/652912/gta-6-cuando-sale-a-la-venta-y-a-que-precio_1440x810_wmk.webp',
+        img4: 'https://www.dexerto.fr/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.dexerto.fr/wp-content/uploads/sites/2/2023/11/06/gta-6-devrait-apporter-changement-armes-majeur-inspire-de-red-dead-redemption.jpg',
+        
+    }
 ];
+
+
 
 
 var filas = JSON.parse(localStorage.getItem('filas'));
